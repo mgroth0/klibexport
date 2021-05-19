2cb2ad2ffc2d93154ffc472d7a4c3baf7266d061
Matt Groth <mgroth49@gmail.com>
2021-05-19 02:32:19 -0400

    Merge branch 'master' of https://github.com/mgroth0/gui
    # Conflicts:
    #	src/main/kotlin/matt/gui/proto/protonodes.kt

--- src/main/kotlin/matt/klibexport/lang/lang.kt
+++ src/main/kotlin/matt/klibexport/lang/lang.kt
@@ -1,5 +1,8 @@
 package matt.klibexport.lang
 
+import kotlin.contracts.InvocationKind.EXACTLY_ONCE
+import kotlin.contracts.contract
+
 
 infix fun <A, B, C> Pair<A, B>.trip(third: C): Triple<A, B, C> {
   return Triple(first, second, third)
@@ -8,3 +11,11 @@ infix fun <A, B, C> Pair<A, B>.trip(third: C): Triple<A, B, C> {
 fun <T> Iterable<T>.applyEach(op: T.()->Unit) = forEach { it.apply(op) }
 
 fun <T> Sequence<T>.onEachApply(op: T.()->Unit) = onEach { it.apply(op) }
+
+inline fun <T: Any> T.alsoPrintln(op: T.()->String): T {
+  contract {
+	callsInPlace(op, EXACTLY_ONCE)
+  }
+  println(op.invoke(this))
+  return this
+}
